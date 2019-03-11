f262d42b054dfa10bf4720bd5ee49bb2493e8426
Philipe Louchtch <philipelouchtch@outlook.com>
2019-03-11 14:16:51 +0100

    improve RdObject with type bounds and comments

--- src/main/scala/rover/rdo/client/RdObject.scala
+++ src/main/scala/rover/rdo/client/RdObject.scala
@@ -1,36 +1,57 @@
 package rover.rdo.client
 
-//import java.security.MessageDigest
-
+//FIXME: use hashes instead of Longs/Strings
 trait RdObject {
-	//FIXME: use hashes instead of Longs/Strings
-	def version: Long
+
+	/**
+	  * The current version of the RDO instance, if current == stable
+	  * then the RDO does not contain any unsaved state changes
+	  * @return The current version of the RDO state
+	  */
+	def currentVersion: Long
+
+	/**
+	  * The persisted (on master) version this RDO instance was based on
+	  * initially.
+	  * @return Version of persisted RDO state the instance has started with
+	  */
 	def stableVersion: Long
 }
 
-class CommonAncestor(one: RdObject, other: RdObject) extends RdObject {
+/**
+  * Encapsulates the concept of a "common ancestor" RDO. That is, given two
+  * RDO instances, the most recent state that both of them share.
+  * This class is also responsible for determining the common ancestor between
+  * the two instances.
+  * @param one Some RDO
+  * @param other Some other RDO
+  */
+class CommonAncestor[+RDO <: RdObject](one: RDO, other: RDO) extends RdObject {
 
 	// determine it once and defer all RdObject methods to it
 	private val commonAncestor: RdObject = {
 		// determine here... & probably cache or is that not needed in scala? :S
-		null
+		// FIXME: proper determination
+		one
 	}
 
-	override def version: Long = {
-		commonAncestor.version
+	override def currentVersion: Long = {
+		commonAncestor.currentVersion
 	}
 
 	override def stableVersion: Long = {
 		commonAncestor.stableVersion
 	}
 
-  def hasDiverged(other: RdObject): Long =  {
-		if (this.version != other.version){
-			commonAncestor.version
+	// FIXME: determine what to do with this, fix return value/type
+    def hasDiverged(other: RdObject): Long =  {
+		// FIXME: non-logical result
+	    if (this.currentVersion != other.currentVersion){
+			commonAncestor.currentVersion
 		}
 		else{
 			println("Non-divergent objects")
-			this.version
+			this.currentVersion
 		}
 	}
 }
