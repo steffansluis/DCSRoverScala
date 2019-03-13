dd9c7c13857fe04ca21393c8eff05e40c4258d68
Philipe Louchtch <philipelouchtch@outlook.com>
2019-03-13 13:14:31 +0100

    simplify AtomicObjectState generic to the actual state container type

--- src/main/scala/rover/rdo/AtomicObjectState.scala
+++ src/main/scala/rover/rdo/AtomicObjectState.scala
@@ -1,22 +1,21 @@
 package rover.rdo
 
-abstract class AtomicObjectState[A <: AtomicObjectState[A]] extends ObjectState {
-	// binding this to a self-type A
-	this: A =>
-
+abstract class AtomicObjectState[A](private var value: A) extends ObjectState {
 	private var ops: List[Op] = List()
 
 	type Op = A => A
 
-	def applyOp(operation: Op): A = {
+	def immutableState: A = value
+
+	def applyOp(operation: Op): Unit = {
 		// Operation must apply itself to the state
 		// but we want the state to take in the operations
 		// so that the framework can record the op
-		val result = operation.apply(this)
+		val result = operation.apply(this.value)
 
 		// Record the operation in the "log" or "event stream" of operations
 		this.ops = this.ops :+ operation
 
-		return result
+		this.value = result
 	}
 }
--- src/main/scala/votingapp/Poll.scala
+++ src/main/scala/votingapp/Poll.scala
@@ -22,29 +22,36 @@ class PollResult(private  val votes: Votes) {
 
 case class PollChoice(choice: String)
 
-class Votes(val asMap: Map[PollChoice, Int]) extends AtomicObjectState[Votes] {
+class Votes(val map: Map[PollChoice, Int]) extends AtomicObjectState[Map[PollChoice, Int]](map) {
 	/**
 	  * Adds the given poll choice to the votes cast. Also: immutable object pattern.
 	  * @param vote The vote-choice to cast
 	  * @return New state with the vote added
 	  */
-	def add(vote: PollChoice): Votes = {
-		return applyOp(new CastVoteOp(vote))
+	def add(vote: PollChoice): Unit = {
+		applyOp(state => {
+			if (state contains vote) {
+				state updated (vote, state(vote) + 1)
+			}
+			else {
+				state updated (vote, 1)
+			}
+		})
 	}
 
 	def majorityChoice: PollChoice = {
 		// FIXME: ties, idea: return a "poll-result" not the choice
-		val winner = this.asMap.maxBy(_._2)._1
+		val winner = this.immutableState.maxBy(_._2)._1
 		return winner
 	}
 
 	override def toString: String = {
-		asMap.toString
+		immutableState.toString
 	}
 }
 
 object Votes {
-	def apply(choices: List[PollChoice]) : Votes = {
+	def apply(choices: List[PollChoice]): Votes= {
 		// TODO: maybe remove, not needed anymore... we accept any choice? Or enforce only valid choices
 		return new Votes(choices.map(choice => (choice,0)).toMap)
 	}
@@ -55,16 +62,16 @@ object Votes {
 }
 
 // app code
-class CastVoteOp(private val vote: PollChoice) extends Votes#Op {
-	final override def apply(state: Votes): Votes = {
-		if (state.asMap contains vote) {
-			Votes(state.asMap updated (vote, state.asMap(vote) + 1))
-		}
-		else {
-			Votes(state.asMap updated (vote, 1))
-		}
-	}
-}
+//class CastVoteOp(private val vote: PollChoice) extends Votes#Op {
+//	final override def apply(state: Votes): Votes = {
+//		if (state.asMap contains vote) {
+//			Votes(state.asMap updated (vote, state.asMap(vote) + 1))
+//		}
+//		else {
+//			Votes(state.asMap updated (vote, 1))
+//		}
+//	}
+//}
 
 object henk {
 	def main(args: Array[String]): Unit = {
