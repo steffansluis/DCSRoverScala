e3cf987042639f35b8e6f8eaba7d80a1e67f0d91
giannislelekas <giannislelekas@gmail.com>
2019-03-14 17:06:29 +0100

    First approach for extracting a common ancestor between RDOs

--- src/main/scala/rover/rdo/AtomicObjectState.scala
+++ src/main/scala/rover/rdo/AtomicObjectState.scala
@@ -2,10 +2,15 @@ package rover.rdo
 
 class AtomicObjectState[A](private var value: A) extends ObjectState {
 	private var ops: List[Op] = List()
+	private var states: List[AtomicObjectState[A]] = List(new AtomicObjectState[A](value))
 
 	type Op = A => A
 
 	def immutableState: A = value
+
+	def opsSize: Long = ops.length
+
+	def getStates: List[AtomicObjectState[A]] =  return states
 
 	def applyOp(operation: Op): Unit = {
 		// Operation must apply itself to the state
@@ -15,6 +20,7 @@ class AtomicObjectState[A](private var value: A) extends ObjectState {
 
 		// Record the operation in the "log" or "event stream" of operations
 		this.ops = this.ops :+ operation
+		this.states = this.states :+ new AtomicObjectState[A](result)
 
 		this.value = result
 	}
