55ee582afb994b88ce8b97c182246c34d6103e54
giannislelekas <giannislelekas@gmail.com>
2019-03-21 11:54:57 +0100

    client-server logic for importing-exporing RDOs within a Session

--- src/main/scala/rover/Client.scala
+++ src/main/scala/rover/Client.scala
@@ -1,19 +1,38 @@
 package rover
 
-class Client[C](serverAddress: String, identifier: Session[C]#Identifier) {
-  val server = Server.fromAddress[C](serverAddress)
+import rover.rdo.AtomicObjectState
 
-  def createSession(credentials: C) = {
+//class Client[C](serverAddress: String, identifier: Session[C]#Identifier) {
+//  val server = Server.fromAddress[C](serverAddress)
+//
+//  def createSession(credentials: C) = {
+//    server.createSession(credentials)
+//  }
+//}
+
+class Client[C, A](private val serverAddress: String, private val id: C, private var mapToStates: Map[String, AtomicObjectState[A]]){
+  private val identifier: Session[C, A]#Identifier = null
+
+  val server = Server.fromAddress[C,A](serverAddress)
+  def createSession(credentials: C): Session[C, A] = {
     server.createSession(credentials)
+  }
+
+  def appended(stateId: String, atomicState: AtomicObjectState[A]): Unit ={
+    this.mapToStates = this.mapToStates + (stateId -> atomicState)
+  }
+
+  def getAtomicStateWithId(stateId: String): AtomicObjectState[A] ={
+    return mapToStates(stateId)
   }
 }
 
 
 object Client {
   class OAuth2Credentials(val accessToken: String, val refreshToken: String) {}
-  type OAuth2Client = Client[OAuth2Credentials]
+  type OAuth2Client[A] = Client[OAuth2Credentials, A]
 
-  def oauth2(): OAuth2Client = {
+  def oauth2[A](): OAuth2Client[A] = {
     null
   }
 }
