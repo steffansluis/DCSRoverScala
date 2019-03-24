cdb735e5b98eff94bb263ce07a49df453c4bdbf2
Philipe Louchtch <philipelouchtch@outlook.com>
2019-03-24 22:49:32 +0100

    refactor & streamline object id's Now the object id's are autogenerated on new rdo's and are kept track in the state of the rdo

--- src/main/scala/rover/rdo/ObjectId.scala
+++ src/main/scala/rover/rdo/ObjectId.scala
@@ -1,4 +1,13 @@
 package rover.rdo
 
+import java.util.UUID
+
 case class ObjectId(asString: String) {
+}
+
+object ObjectId {
+	def generateNew(): ObjectId = {
+		val uuid = UUID.randomUUID()
+		return ObjectId(uuid.toString())
+	}
 }
