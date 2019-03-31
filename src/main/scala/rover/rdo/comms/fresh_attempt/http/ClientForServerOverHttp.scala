a94e390497d50c8bc677f483622a7987d6984c43
Philipe Louchtch <philipelouchtch@outlook.com>
2019-03-31 23:01:32 +0200

    fresh client-server attempt

--- src/main/scala/rover/rdo/comms/fresh_attempt/http/ClientForServerOverHttp.scala
+++ src/main/scala/rover/rdo/comms/fresh_attempt/http/ClientForServerOverHttp.scala
@@ -1,11 +1,43 @@
 package rover.rdo.comms.fresh_attempt.http
 
+import com.mashape.unirest.http.Unirest
 import rover.rdo.ObjectId
 import rover.rdo.comms.fresh_attempt.Client
 import rover.rdo.state.AtomicObjectState
 
-class ClientForServerOverHttp[A <: Serializable] extends Client[A]{
-	override def fetch(objectId: ObjectId): Unit = ???
+/**
+  * Provides a simple to use API to communicate to a state/object server
+  * over HTTP.
+  *
+  * @note This class could have extended the ServerHttpInterface "interface",
+  *       but  doing so has potential to cause confusion. As well as forcing
+  *       server-side method naming onto the client (accept vs push). Can be
+  *       overcome in the future with better naming (both interface as API)
+  * @param endpointPaths
+  * @tparam A The state itself
+  */
+class ClientForServerOverHttp[A <: Serializable](
+	private val endpointPaths: ServerHttpEndpointPaths
+) extends Client[A]{
 
-	override def push(state: AtomicObjectState[A]): Unit = ???
+	override def created(): AtomicObjectState[A] = {
+		val createResponse = Unirest.get(endpointPaths.createEndpoint).asBinary()
+		val received = DeserializedAtomicObjectState[A](createResponse.getRawBody).asAtomicObjectState
+
+		return received
+	}
+
+	override def fetch(objectId: ObjectId): AtomicObjectState[A] = {
+		val fetchResponse = Unirest.get(endpointPaths.getEndpoint).asBinary()
+		val deserialized = DeserializedAtomicObjectState[A](fetchResponse.getRawBody)
+
+		return deserialized.asAtomicObjectState
+	}
+
+	override def push(state: AtomicObjectState[A]): Unit = {
+		val serialized = new SerializedAtomicObjectState[A](state)
+		Unirest.post(endpointPaths.acceptEndpoint).body(serialized.asBytes)
+
+		// TODO: ?
+	}
 }
--- src/main/scala/rover/rdo/comms/fresh_attempt/http/ServerHttpInterface.scala
+++ src/main/scala/rover/rdo/comms/fresh_attempt/http/ServerHttpInterface.scala
@@ -4,16 +4,41 @@ import rover.rdo.ObjectId
 import rover.rdo.comms.fresh_attempt.Server
 import spark.Spark._
 
+/**
+  * Provides a default HTTP "restful" endpoints for `Server[A]` implementations
+  *
+  * @note Not statistically checked during compile-time if implementation is complete.
+  *       Could use Spring's RestControllers or other method-based frameworks instead
+  *       of Spark to be able to achieve this.
+  * @param applicationName The name of the object/state. Is used as part of the http path
+  * @param serverImpl The underlying server implementation
+  * @tparam A
+  */
 class ServerHttpInterface[A <: Serializable](
 	private val applicationName: String,
 	private val serverImpl: Server[A]
 ) {
+	val endpointPaths = ServerHttpEndpointPaths.forApp(applicationName)
+
+	get(endpointPaths.createEndpoint, (request, result) => {
+		val newlyCreated = serverImpl.create()
+
+		val serialized = new SerializedAtomicObjectState[A](newlyCreated)
+
+		result.body(serialized.asString)
+		result.`type`("application/octet-stream")
+
+		result
+	})
+
 	// Create an endpoint for the "get" server method
-	get(s"$applicationName/get/:objectId", (request, result) => {
+	get(endpointPaths.getEndpoint, (request, result) => {
 		val objectIdStringInRequestParam = request.params(":objectId")
 		val objectId = ObjectId.from(objectIdStringInRequestParam)
 
 		val latestOnServer = serverImpl.get(objectId)
+			.getOrElse(throw new Exception("Server did not have requested object/state..."))
+
 		val serializedState = new SerializedAtomicObjectState[A](latestOnServer)
 
 		result.body(serializedState.asString)
@@ -22,9 +47,9 @@ class ServerHttpInterface[A <: Serializable](
 		result
 	})
 
-	post(s"$applicationName/accept", (request, result) => {
+	post(endpointPaths.acceptEndpoint, (request, result) => {
 		val bytes = request.bodyAsBytes()
-		val deserializedAtomicObjectState = new DeserializedAtomicObjectState[A](bytes)
+		val deserializedAtomicObjectState = new AtomicObjectStateAsByteArray[A](bytes)
 
 		val incomingState = deserializedAtomicObjectState.asAtomicObjectState
 		serverImpl.accept(incomingState)
@@ -33,10 +58,23 @@ class ServerHttpInterface[A <: Serializable](
 		result
 	})
 
-	get(s"$applicationName/status", (request, result) => {
+	get(endpointPaths.statusEndpoint, (request, result) => {
 		// TODO
 		result.status(200)
 
 		result
 	})
+}
+
+final case class ServerHttpEndpointPaths private (private val applicationName: String) {
+	def createEndpoint = s"$applicationName/create"
+	def getEndpoint = s"$applicationName/get/:objectId"
+	def acceptEndpoint = s"$applicationName/accept"
+	def statusEndpoint = s"$applicationName/status"
+}
+
+object ServerHttpEndpointPaths {
+	def forApp(appName: String): ServerHttpEndpointPaths = {
+		return new ServerHttpEndpointPaths(appName)
+	}
 }
